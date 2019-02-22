85f887ef6f26338821e3a8f2fcb727b92733814a
Maxence RIOU <rioumaxe@gmail.com>
2019-02-22 17:37:32 +0100

    ajout testArgParser

---
+++ tests/blackboxTesting/testArgParser.py
@@ -0,0 +1,37 @@
+import unittest
+import sys
+
+sys.path.append("../../src/")
+
+from argParser import Parser
+
+class testArgParser(unittest.TestCase):
+
+    def testcheckbervalidity(self):
+        parser = Parser()
+        ber = -1
+        self.assertFalse(parser.checkbervalidity(ber))
+        ber = 121921
+        self.assertFalse(parser.checkbervalidity(ber))
+        ber = 0
+        self.assertTrue(parser.checkbervalidity(ber))
+        ber = 1
+        self.assertTrue(parser.checkbervalidity(ber))
+        ber = 0.00043
+        self.assertTrue(parser.checkbervalidity(ber))
+
+    def testcheckpayloadsizevalidity(self):
+        parser = Parser()
+        payloadsize = -1
+        self.assertFalse(parser.checkpayloadsizevalidity(payloadsize))
+        payloadsize = 1600
+        self.assertFalse(parser.checkpayloadsizevalidity(payloadsize))
+        payloadsize = 0
+        self.assertTrue(parser.checkpayloadsizevalidity(payloadsize))
+        payloadsize = 1
+        self.assertTrue(parser.checkpayloadsizevalidity(payloadsize))
+        payloadsize = 20
+        self.assertTrue(parser.checkpayloadsizevalidity(payloadsize))
+
+if __name__ == '__main__':
+    unittest.main()
