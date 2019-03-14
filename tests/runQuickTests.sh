3a8b7a9ac04b19a6156c0fa2b1301a6f8d896921
rrhardy <robin.hardy@etu.u-bordeaux.fr>
2019-03-14 16:34:47 +0100

    reorganized test directory

--- tests/runQuickTests.sh
+++ tests/runQuickTests.sh
@@ -3,8 +3,8 @@ dir="${0%/*}"
 cd "$dir"/unitTest
 #python3 -m unittest quick
 
-#python3 -m unittest testArgParser
-#python3 -m unittest testController
-#python3 -m unittest testFrame
-#python3 -m unittest testSimulation
-#python3 -m unittest testRandomSeriesSimulation
+python3 -m unittest testArgParser
+python3 -m unittest testController
+python3 -m unittest testFrame
+python3 -m unittest testSimulation
+python3 -m unittest testRandomSeriesSimulation
