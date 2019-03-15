14aca63e97b94c304697222a9d509944b7e137f2
Maxence RIOU <rioumaxe@gmail.com>
2019-03-15 17:41:13 +0100

    fix erreur -p dans simulWindows.sh

--- simulWindows.sh
+++ simulWindows.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
-#BER fixe, PAYLOAD variable :
-#./simul -p <data> <ber> <payload_min> <payload_max><pas>
-#Exemple :
-#./simul -p 10G 0.001 50 400 20
-
-#PAYLOAD fixe, BER variable (pas fixe) : 
-#./simul -b <data> <payload> <ber_min> <ber_max>
-#Exemple :
-#./simul -b 10G 500 0.00001 0.01
-
-# store arguments in an array 
-args=("$@") 
-# get number of elements 
-ELEMENTS=${#args[@]} 
 
 if [ $# -eq 0 ]; then
     echo "- Usage 1 -> ber fixe, payload variable :"
-    echo " ./simul.sh -p <taille données> <BER> <payload_min> <payload_max> <pas>"
+    echo " ./simul.sh -p <taille donnees> <BER> <payload_min> <payload_max> <pas>"
     echo "- Usage 2 -> payload fixe, ber variable :"
-    echo " ./simul.sh -b <taille données> <payload> <ber_min> <ber_max>"
+    echo " ./simul.sh -b <taille donnees> <payload> <ber_min> <ber_max>"
     exit
 fi
 
@@ -26,56 +12,55 @@ rm -f results.txt
 echo "Starting simulation..."
 
 if [ $1 == "-p" ]; then
-echo "Usage 1 : ber fixe, payload variable"
-	
-step=$6
-
-for (( payload=$payload_min;payload<=	$payload_max;payload+=$step)); do
-	cd src/
-	results=$(python main.py -q -P $payload -r $data 	$ber)
-	read results< <(echo "$results" | tail -n1)
-	echo $results
-	cd ../
-	touch results.txt
-	results="$results \n"
-	echo $results$'\r' >> results.txt
-done
-gnuplot -p -e "plot 'results.txt' u 3:4 w l"
+	echo "Usage 1 : ber fixe, payload variable"
+	payload_min=$4
+	payload_max=$5
+	step=$6
+	for ((payload=$payload_min;payload<=$payload_max;payload+=$step)); do
+		cd src/
+		results=$(python main.py -q -P $payload -r $data $ber)
+		read results< <(echo "$results" | tail -n1)
+		echo $results
+		cd ../
+		touch results.txt
+		results="$results \n"
+		echo $results$'\r' >> results.txt
+	done
+	#gnuplot -p -e "plot 'results.txt' u 3:4 w l"
 fi
 
 if [ $1 == "-b" ]; then
-echo "Usage 2 : payload fixe, ber variable"
-data=$2
-payload=$3
-ber_min=$4
-ber_max=$5
+	echo "Usage 2 : payload fixe, ber variable"
+	data=$2
+	payload=$3
+	ber_min=$4
+	ber_max=$5
+	ber=$ber_min
+	step=`echo $ber_max 10 | awk '{print $1 / $2}'`
 
-ber=$ber_min
-step=`echo $ber_max 10 | awk '{print $1 / $2}'`
-
-#multiplication pour passer le while
-ber=`echo $ber 1000000000 | awk '{print $1 * $2}'`
-ber_max=`echo $ber_max 1000000000 | awk '{print $1 * $2}'`
-
-while [ $ber -lt $ber_max ]
-do
-	#division pour reprendre vraies valeurs
-	ber=`echo $ber 1000000000 | awk '{print $1 / $2}'`
-	ber_max=`echo $ber_max 1000000000 | awk '{print $1 / $2}'`
-		ber=`echo $ber $step | awk '{print $1 + $2}'`
-		echo $ber
-	#cd src/
-	#results=$(python main.py -q -P $payload -r $data $ber)
-	#read results< <(echo "$results" | tail -n1)
-	#echo $results
-	#cd ../
-	#touch results.txt
-	#results="$results \n"
-#	echo $results$'\r' >> results.txt
+	#multiplication pour passer le while
 	ber=`echo $ber 1000000000 | awk '{print $1 * $2}'`
 	ber_max=`echo $ber_max 1000000000 | awk '{print $1 * $2}'`
-done
-#gnuplot -p -e "plot 'results.txt' u 3:4 w l"
+
+	while [ $ber -lt $ber_max ]
+	do
+		#division pour reprendre vraies valeurs
+		ber=`echo $ber 1000000000 | awk '{print $1 / $2}'`
+		ber_max=`echo $ber_max 1000000000 | awk '{print $1 / $2}'`
+		ber=`echo $ber $step | awk '{print $1 + $2}'`
+		echo $ber
+		#cd src/
+		#results=$(python main.py -q -P $payload -r $data $ber)
+		#read results< <(echo "$results" | tail -n1)
+		#echo $results
+		#cd ../
+		#touch results.txt
+		#results="$results \n"
+		#echo $results$'\r' >> results.txt
+		ber=`echo $ber 1000000000 | awk '{print $1 * $2}'`
+		ber_max=`echo $ber_max 1000000000 | awk '{print $1 * $2}'`
+	done
+	#gnuplot -p -e "plot 'results.txt' u 3:4 w l"
 fi
 
 echo "done!"
