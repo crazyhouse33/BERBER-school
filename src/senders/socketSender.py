9dbf72e865dedfa25711c2f6777396d9cf34d22d
huitredelombre <hugo.alonso@etu.u-bordeaux.fr>
2019-03-15 14:45:45 +0100

    coriged a bug with socket

--- src/senders/socketSender.py
+++ src/senders/socketSender.py
@@ -3,8 +3,8 @@ from senders.scapySender import ScapySender
 import socket
 class SocketSender(ScapySender):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self,headerSize):
+        super().__init__(headerSize)
         self.sock= socket.socket( socket.AF_PACKET,socket.SOCK_RAW)
         self.sock.bind(('lo',0))
         
