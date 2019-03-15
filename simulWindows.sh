e68f14f5965d1a54089a769e6a9d34a37faa3726
Maxence RIOU <rioumaxe@gmail.com>
2019-03-15 16:15:21 +0100

    maj simulWindows.sh while fonctionne avec floats

--- simulWindows.sh
+++ simulWindows.sh
@@ -51,21 +51,31 @@ ber_min=$4
 ber_max=$5
 
 ber=$ber_min
-step=0.00005
+step=`echo $ber_max 10 | awk '{print $1 / $2}'`
+
+#multiplication pour passer le while
+ber=`echo $ber 1000000000 | awk '{print $1 * $2}'`
+ber_max=`echo $ber_max 1000000000 | awk '{print $1 * $2}'`
 
 while [ $ber -lt $ber_max ]
 do
-	ber=`echo $ber + $step | bc`
-	cd src/
-	results=$(python main.py -q -P $payload -r $data $ber)
-	read results< <(echo "$results" | tail -n1)
-	echo $results
-	cd ../
-	touch results.txt
-	results="$results \n"
-	echo $results$'\r' >> results.txt
+	#division pour reprendre vraies valeurs
+	ber=`echo $ber 1000000000 | awk '{print $1 / $2}'`
+	ber_max=`echo $ber_max 1000000000 | awk '{print $1 / $2}'`
+		ber=`echo $ber $step | awk '{print $1 + $2}'`
+		echo $ber
+	#cd src/
+	#results=$(python main.py -q -P $payload -r $data $ber)
+	#read results< <(echo "$results" | tail -n1)
+	#echo $results
+	#cd ../
+	#touch results.txt
+	#results="$results \n"
+#	echo $results$'\r' >> results.txt
+	ber=`echo $ber 1000000000 | awk '{print $1 * $2}'`
+	ber_max=`echo $ber_max 1000000000 | awk '{print $1 * $2}'`
 done
-gnuplot -p -e "plot 'results.txt' u 3:4 w l"
+#gnuplot -p -e "plot 'results.txt' u 3:4 w l"
 fi
 
 echo "done!"
