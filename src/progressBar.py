01d0ed7dd2a0894cd200d9652bdc3650fa18b567
huitredelombre <hugo.alonso@etu.u-bordeaux.fr>
2019-03-19 11:35:06 +0100

    eliminated error tracing when keyboardInterupt

--- src/progressBar.py
+++ src/progressBar.py
@@ -1,25 +1,39 @@
-#from : https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+# from :
+# https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+
+
 class ProgressBar():
 
-    def __init__(self, end, prefix='', suffix='', decimals = 1, length = 50, fill = '█'):
-        self.end=end
-        self.prefix=prefix
-        self.suffix=suffix
-        self.decimals=decimals
-        self.length=length
-        self.fill=fill
+    def __init__(
+            self, end, prefix='', suffix='', decimals=1, length=50, fill='█'):
+        self.end = end
+        self.prefix = prefix
+        self.suffix = suffix
+        self.decimals = decimals
+        self.length = length
+        self.fill = fill
 
-    def update(self, iteration ):
+    def update(self, iteration):
         """return True if the end was reached"""
         """avoiding race condition here which can lead to crash if payloadsize=0"""
-        protectedEnd=self.end
-        if protectedEnd==0:
-            iteration=1
-            protectedEnd=1
-        percent = ("{0:." + str(self.decimals) + "f}").format(100 * (iteration / float(protectedEnd)))
+        protectedEnd = self.end
+        if protectedEnd == 0:
+            iteration = 1
+            protectedEnd = 1
+        percent = ("{0:." +
+                   str(self.decimals) +
+                   "f}").format(100 *
+                                (iteration /
+                                 float(protectedEnd)))
         filledLength = int(self.length * iteration // protectedEnd)
         bar = self.fill * filledLength + '-' * (self.length - filledLength)
-        print('\r%s |%s| %s%% %s' % (self.prefix, bar, percent, self.suffix), end = '')
-        if iteration == protectedEnd: 
+        print(
+            '\r%s |%s| %s%% %s' %
+            (self.prefix,
+             bar,
+             percent,
+             self.suffix),
+            end='')
+        if iteration == protectedEnd:
             return False
         return True
